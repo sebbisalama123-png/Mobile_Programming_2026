811a7c6f8417e9439e15ce970c8161bcf74bf35e
Sebbi Salama <sebbisalama123@gmail.com>
2026-03-07 13:46:08 +0300

    student card commit

--- app/src/main/java/ug/ac/ndejje/welcome/MainActivity.kt
+++ app/src/main/java/ug/ac/ndejje/welcome/MainActivity.kt
@@ -1,17 +1,41 @@
 package ug.ac.ndejje.welcome
 
+import android.R.attr.text
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ug.ac.ndejje.welcome.ui.theme.Ndejje_Welcome_AppTheme
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +43,88 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Ndejje_Welcome_AppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Salama",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                StudentIdCard()
                 }
             }
         }
     }
-}
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun StudentInfo() {
+    val profileImage = painterResource(R.drawable.student_photo)
+    val logoImage = painterResource(R.drawable.ndu_logo)
+    Column(
+        modifier = Modifier.padding(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box {
+            Image(
+                painter = profileImage,
+                contentDescription = "Student Photo",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.clip(RoundedCornerShape(percent = 10))
+            )
+            Image(
+                painter = logoImage,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(80.dp)
+                    .align(Alignment.BottomEnd)
+                    .padding(4.dp)
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = stringResource(R.string.student_name),
+            style = MaterialTheme.typography.headlineSmall,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.primary
+        )
+        Text(
+            text = stringResource(R.string.programme),
+            style = MaterialTheme.typography.labelMedium,
+            color = MaterialTheme.colorScheme.secondary,
+            modifier = Modifier.padding(4.dp)
+        )
+        HorizontalDivider(
+            modifier = Modifier.padding(vertical = 12.dp, horizontal = 24.dp)
+        )
+        Row {
+            Text(
+                text = "REG NO: ",
+                style = MaterialTheme.typography.labelMedium,
+                color = MaterialTheme.colorScheme.secondary,
+                modifier = Modifier.padding(4.dp)
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            Text(
+                text = stringResource(R.string.reg_number),
+                style = MaterialTheme.typography.labelMedium,
+                color = MaterialTheme.colorScheme.secondary,
+                modifier = Modifier.padding(4.dp)
+                )
+        }
+    }
 }
-
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Ndejje_Welcome_AppTheme {
-        Greeting("Salama")
+fun StudentIdCard() {
+    ElevatedCard(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        shape = RoundedCornerShape(16.dp),
+        colors = CardDefaults.elevatedCardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant.copy(0.3f)
+        )
+    ) {
+        StudentInfo()
+    }
+}
+@Preview(
+    showBackground = true,
+    showSystemUi = true)
+@Composable
+fun WelcomePreview() {
+    Ndejje_Welcome_AppTheme() {
+        StudentIdCard()
     }
 }
